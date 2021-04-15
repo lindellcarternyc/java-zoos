606d929265a88c9421c0305b3386a5b3b28143fb
Lindell Carter <lindellcarternyc@gmail.com>
2021-04-15 17:56:04 -0400

    setup project

---
+++ src/test/java/com/example/javazoos/JavaZoosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javazoos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaZoosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
